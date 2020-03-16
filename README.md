63da4f220515d48d7919631644e8df79c7bd4e36
Pavel Zavora <pavel.zavora@bonitoo.io>
2020-03-16 18:07:26 +0100

    feat(cmd): add cobra cmd line runner

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 CSV import to INFLUXDB
 
 See https://github.com/influxdata/influxdb/issues/17003
+
+## Resources
+* https://towardsdatascience.com/how-to-create-a-cli-in-golang-with-cobra-d729641c7177
---
+++ cmd/import.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// importCmd represents the import command
+var importCmd = &cobra.Command{
+	Use:   "import",
+	Short: "Imports CSV files into InfluxDB",
+	Long:  `Imports CSV (query result) files into InfluxDB.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("TODO run import")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(importCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// importCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// importCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
